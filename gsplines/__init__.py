e2eb01ddec32fdb8730f85f11782bc16ec94f812
Rojas Rafael <rafael.rojas@unibz.it>
2020-07-17 10:44:40 +0200

    json in gsplines.__init__

--- gsplines/__init__.py
+++ gsplines/__init__.py
@@ -3,3 +3,6 @@ from .gspline import cSplineCalc
 from .basis0010 import cBasis0010
 from .basis1010 import cBasis1010
 from .basis1000 import cBasis1000
+
+from .json import piecewise2json
+from .json import json2piecewise
